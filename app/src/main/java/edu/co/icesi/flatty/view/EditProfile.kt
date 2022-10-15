13021e686b6ec8e12f35965344199ec032c12831
julian-b24 <julian.camilob24@gmail.com>
2022-10-15 14:25:04 -0500

    Fonts deleted

--- app/src/main/java/edu/co/icesi/flatty/view/EditProfile.kt
+++ app/src/main/java/edu/co/icesi/flatty/view/EditProfile.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class EditProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_edit_profile)
+        setContentView(R.layout.activity_edit_profile)
     }
 }
