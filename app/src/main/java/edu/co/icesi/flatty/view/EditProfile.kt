890618d0c6a34b54eff9039ab5157e2f9781f58b
julian-b24 <julian.camilob24@gmail.com>
2022-10-15 16:33:24 -0500

    Font render bug fixed and User image added

--- app/src/main/java/edu/co/icesi/flatty/view/EditProfile.kt
+++ app/src/main/java/edu/co/icesi/flatty/view/EditProfile.kt
@@ -2,10 +2,16 @@ package edu.co.icesi.flatty.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.co.icesi.flatty.databinding.ActivityEditProfileBinding
 
 class EditProfile : AppCompatActivity() {
+
+    val binding: ActivityEditProfileBinding by lazy {
+        ActivityEditProfileBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        setContentView(binding.root)
     }
 }
