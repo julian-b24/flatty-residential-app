1a2f1145e96ce62ba9cd26573d5acbd57e550497
Juan Camilo <61602045+juanmiloz@users.noreply.github.com>
2022-10-18 20:16:45 -0500

    fix conflix

--- app/src/main/java/edu/co/icesi/flatty/view/ProfileSearchedPage.kt
+++ app/src/main/java/edu/co/icesi/flatty/view/ProfileSearchedPage.kt
@@ -3,8 +3,6 @@ package edu.co.icesi.flatty.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import edu.co.icesi.flatty.R
-import edu.co.icesi.flatty.databinding.ActivityEditProfileBinding
 import edu.co.icesi.flatty.databinding.ActivityProfileSearchedPageBinding
 
 class ProfileSearchedPage : AppCompatActivity() {
