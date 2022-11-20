7bfad14e766f225f0f63d8b0fbf0389d7a571759
julian-b24 <julian.camilob24@gmail.com>
2022-11-20 15:16:12 -0500

    Resident is able to add favourites persons

--- app/src/main/java/edu/co/icesi/flatty/view/FavouritesListResidentActivity.kt
+++ app/src/main/java/edu/co/icesi/flatty/view/FavouritesListResidentActivity.kt
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.LinearLayout
+import android.widget.*
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import edu.co.icesi.flatty.R
 import edu.co.icesi.flatty.databinding.ActivityFavouritesListResidentBinding
 import edu.co.icesi.flatty.databinding.AddVehicleBottomSheetBinding
+import edu.co.icesi.flatty.model.FavouritePerson
+import edu.co.icesi.flatty.model.FavouriteVehicle
 import edu.co.icesi.flatty.model.VehicleType
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class FavouritesListResidentActivity : AppCompatActivity() {
+
+    val residentId: String = Firebase.auth.currentUser?.uid.toString()
 
     val binding:ActivityFavouritesListResidentBinding by lazy{
         ActivityFavouritesListResidentBinding.inflate(layoutInflater)
@@ -34,17 +44,7 @@ class FavouritesListResidentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        layoutManagerFavouritePerson = GridLayoutManager(this,2)
-        binding.personsRV.layoutManager = layoutManagerFavouritePerson
-        binding.personsRV.setHasFixedSize(true)
-        adapterFavouritePerson = FavouritePersonAdapter()
-        binding.personsRV.adapter = adapterFavouritePerson
-
-        layoutManagerFavouriteVehicle = GridLayoutManager(this, 2)
-        binding.vehiclesRV.layoutManager = layoutManagerFavouriteVehicle
-        binding.vehiclesRV.setHasFixedSize(true)
-        adapterFavouriteVehicle = FavouriteVehicleAdapter()
-        binding.vehiclesRV.adapter = adapterFavouriteVehicle
+        getResidentFavourites()
 
         binding.addPersonBtn.setOnClickListener{
             val bottomSheetDialog = BottomSheetDialog(
@@ -54,6 +54,21 @@ class FavouritesListResidentActivity : AppCompatActivity() {
                 R.layout.add_person_bottom_sheet,
                 findViewById(R.id.addPersonBottomSheet) as LinearLayout?
             )
+
+            bottomSheetView.findViewById<Button>(R.id.add_new_personBtn).setOnClickListener {
+                val name = bottomSheetView.findViewById<TextView>(R.id.newPersonNameET).text.toString()
+                val number = bottomSheetView.findViewById<TextView>(R.id.newPersonIdET).text.toString()
+
+                if(name.isNotEmpty() && number.isNotEmpty()){
+                    addPersonToFavourites(name, number)
+
+                    bottomSheetView.findViewById<TextView>(R.id.newPersonNameET).text = ""
+                    bottomSheetView.findViewById<TextView>(R.id.newPersonIdET).text = ""
+                }else{
+                    Toast.makeText(this, "Campos vacíos", Toast.LENGTH_LONG).show()
+                }
+
+            }
 
             bottomSheetDialog.setContentView(bottomSheetView)
             bottomSheetDialog.show()
@@ -97,6 +112,83 @@ class FavouritesListResidentActivity : AppCompatActivity() {
             finish()
         }
 
+
+    }
+
+    private fun addPersonToFavourites(name: String, number: String) {
+        lifecycleScope.launch(Dispatchers.IO){
+
+            val newPersonReference = Firebase.firestore.collection("residents")
+                .document(residentId)
+                .collection("persons")
+                .document()
+
+            val newPersonId = newPersonReference.id
+            val newPerson = FavouritePerson(newPersonId, name, number)
+
+            newPersonReference.set(newPerson)
+
+            withContext(Dispatchers.Main){
+                adapterFavouritePerson.addFavouritePerson(newPerson)
+                Toast.makeText(this@FavouritesListResidentActivity, "Nueva persona añadida", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+    private fun getResidentFavourites() {
+        lifecycleScope.launch(Dispatchers.IO){
+
+            //Request documents from Firebase
+            val resultVehicles = Firebase.firestore
+                .collection("residents")
+                .document(residentId)
+                .collection("vehicles")
+                .get()
+                .await()
+
+            val resultPersons = Firebase.firestore
+                .collection("residents")
+                .document(residentId)
+                .collection("persons")
+                .get()
+                .await()
+
+            //Parse documents into list
+            var vehicles = arrayListOf<FavouriteVehicle>()
+            for (doc in resultVehicles){
+                val vehicle = doc.toObject(FavouriteVehicle::class.java)
+                vehicles.add(vehicle)
+            }
+
+            var persons = arrayListOf<FavouritePerson>()
+            for (doc in resultPersons){
+                val person = doc.toObject(FavouritePerson::class.java)
+                persons.add(person)
+            }
+
+            withContext(Dispatchers.Main){
+                setResidentFavourites(vehicles, persons)
+            }
+        }
+    }
+
+    private fun setResidentFavourites(vehicles: ArrayList<FavouriteVehicle>, persons: ArrayList<FavouritePerson>){
+        adapterFavouritePerson = FavouritePersonAdapter()
+        adapterFavouriteVehicle = FavouriteVehicleAdapter()
+
+        adapterFavouritePerson.favouritePersonsList = persons
+        adapterFavouriteVehicle.favouriteVehicleList = vehicles
+
+        layoutManagerFavouritePerson = GridLayoutManager(this,2)
+        binding.personsRV.layoutManager = layoutManagerFavouritePerson
+        binding.personsRV.setHasFixedSize(true)
+        binding.personsRV.adapter = adapterFavouritePerson
+
+        layoutManagerFavouriteVehicle = GridLayoutManager(this, 2)
+        binding.vehiclesRV.layoutManager = layoutManagerFavouriteVehicle
+        binding.vehiclesRV.setHasFixedSize(true)
+        binding.vehiclesRV.adapter = adapterFavouriteVehicle
 
     }
 }
--- app/src/main/java/edu/co/icesi/flatty/view/ResidentProfileFragment.kt
+++ app/src/main/java/edu/co/icesi/flatty/view/ResidentProfileFragment.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -34,6 +35,7 @@ class ResidentProfileFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         residentId = Firebase.auth.currentUser!!.uid
+        Log.e(">>>", residentId)
         getResident(residentId)
     }
 
