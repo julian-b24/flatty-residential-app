ec5aa82f10663b1a089bf167f2ed14fab12425bd
Samuelguerrero1184 <samuel040902@hotmail.com>
2022-11-20 23:22:38 -0500

    prueba vehiculos

--- app/src/main/java/edu/co/icesi/flatty/view/FavouritesListResidentActivity.kt
+++ app/src/main/java/edu/co/icesi/flatty/view/FavouritesListResidentActivity.kt
@@ -65,7 +65,6 @@ class FavouritesListResidentActivity : AppCompatActivity() {
                 }else{
                     Toast.makeText(this, "Campos vacíos", Toast.LENGTH_LONG).show()
                 }
-
             }
 
             bottomSheetDialog.setContentView(bottomSheetView)
